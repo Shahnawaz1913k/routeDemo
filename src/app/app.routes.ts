42e5a99606261af3083c878f3d89b2976ce2f647
Shahnawaz alam <sa467546@gmail.com>
2025-08-14 22:48:34 +0530

    Initial Commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
+// app.route.ts
 import { Routes } from '@angular/router';
+import { About } from './about/about';
+import { Login } from './login/login';
+import { Contact } from './contact/contact';
+import { App } from './app';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' }, // default route
+  { path: 'about', component: About },
+  { path: 'login', component: Login },
+  { path: 'contact', component: Contact },
+  { path: '**', redirectTo: '/login' } // wildcard fallback (optional)
+];
